3aa180187d84a3e0f66194997ab9be17c9d9b50c
Anurag Porripireddi <anuragprdi@google.com>
2020-03-23 19:03:57 -0700

    Prioritise LACP select over backup
    Change-Id: Icbe82783d55e6894800704fe4940ae5989f9cc92

--- kokoro/pkg_build.sh
+++ kokoro/pkg_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-benz build --git -pool="rodete-huge" -sign -confirm -branch="gmaster" -target-prefix=enterprise-sdn-faucet-core rpc://perry-internal/faucet
+benz build --git -pool="rodete-huge" -sign -confirm -branch="gmaster_lacp" -target-prefix=enterprise-sdn-faucet-core rpc://perry-internal/faucet
