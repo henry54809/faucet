748b2d52cadb20f3b003b762e56e364d0a5c90f7
Anurag Porripireddi <anuragprdi@google.com>
2020-03-26 08:29:03 -0700

    build pkg change for branch
    Change-Id: I393bd1e943190b03dc57f3d76635e25af3deca59

--- kokoro/pkg_build.sh
+++ kokoro/pkg_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-benz build --git -pool="rodete-huge" -sign -confirm -branch="gmaster" -target-prefix=enterprise-sdn-faucet-core rpc://perry-internal/faucet
+benz build --git -pool="rodete-huge" -sign -confirm -branch="gmaster_mab" -target-prefix=enterprise-sdn-faucet-core rpc://perry-internal/faucet
