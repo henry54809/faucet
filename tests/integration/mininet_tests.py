06ad918695b57b27c4d58203e6fb6729fbe2130a
Anurag Porripireddi <anuragprdi@google.com>
2020-09-21 12:48:18 +0000

    Rename test

--- tests/integration/mininet_tests.py
+++ tests/integration/mininet_tests.py
@@ -3412,7 +3412,7 @@ class FaucetConfigReloadAclTest(FaucetConfigReloadTestBase):
         self.verify_tp_dst_blocked(5003, first_host, second_host)
 
 
-class FaucetConfigReloadMACExpireTest(FaucetConfigReloadTestBase):
+class FaucetConfigReloadMACFlushTest(FaucetConfigReloadTestBase):
 
     def test_port_change_vlan(self):
         self.ping_all_when_learned()
